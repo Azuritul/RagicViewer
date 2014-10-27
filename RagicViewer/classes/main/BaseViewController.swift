8fdbbcb9762fb878e6dabc7be81771e7331a21e7
Azuritul <azuritul@gmail.com>
2014-10-27 15:46:48 +0800

    Added BaseViewController, for holding common logic for all main view controllers.

---
+++ RagicViewer/classes/main/BaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BaseViewController.swift
+//  RagicViewer
+//
+//  Created by azuritul on 2014/10/27.
+//  Copyright (c) 2014年 Labcule. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
