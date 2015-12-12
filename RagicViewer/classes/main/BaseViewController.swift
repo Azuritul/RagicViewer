a59baed157be1dae9213e58b0ad320d788ec2917
Azuritul <azuritul@gmail.com>
2015-12-13 01:50:34 +0900

    Test push using keys.

--- RagicViewer/classes/main/BaseViewController.swift
+++ RagicViewer/classes/main/BaseViewController.swift
@@ -24,21 +24,7 @@ class BaseViewController: UIViewController{
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        if self.dataArray.count == 0 {
-//            let messageLabel = UILabel(frame: CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height))
-//            messageLabel.text = "There is currently no data."
-//            messageLabel.textColor = UIColor.blackColor()
-//            messageLabel.numberOfLines = 0
-//            messageLabel.textAlignment = .Center
-//            messageLabel.font = UIFont(name: "Palatino-Italic", size: 20)
-//            messageLabel.sizeToFit()
-//            self.tableView?.backgroundView = messageLabel
-//            self.tableView?.separatorStyle = .None
-//            return 0
-//        } else {
-//            self.tableView?.separatorStyle = .SingleLine
-//            self.tableView?.backgroundView = nil
-//        }
+
         return 1
     }
 
